6bbf71fafea6b8467bce33e1223754f271dec8e1
Tyler Lawrence <58041173+tyTheDebugger@users.noreply.github.com>
2020-03-04 17:54:00 -0600

    Update setup-P2.sh

--- setup-P2.sh
+++ setup-P2.sh
@@ -12,15 +12,15 @@ unzip -o release-*.zip -d ./raspb0x
 rm release-*.zip
 echo Setting up autostart...
 cd ~
-echo cd /home/pi/raspb0x >> .bashrc
-echo bash -c './start.sh' >> .bashrc
-echo Installing checkra1n dependencies and latest version ATM... deps are the only needed thing.
+echo 'cd /home/pi/raspb0x' >> .bashrc
+echo 'bash -c \'./start.sh\'' >> .bashrc
+echo 'Installing checkra1n dependencies and latest version ATM... deps are the only needed thing.'
 echo "deb https://assets.checkra.in/debian /" | sudo tee -a /etc/apt/sources.list
 sudo apt-key adv --fetch-keys https://assets.checkra.in/debian/archive.key
 sudo apt update
 sudo apt-get install checkra1n
-echo Please navigate to Boot Options and next to Console Auto-Login and enable it.
+echo 'Please navigate to Boot Options and next to Console Auto-Login and enable it.'
 read -n 1 -s -r -p "Press any key to continue, it will reboot when done."
 sudo raspi-config
-echo Reboot needed to complete setup.
+echo 'Reboot needed to complete setup.'
 sudo reboot
