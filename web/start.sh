267a3645d1bd371aceb0cc47df37bf53925e921b
Tyler Lawrence <58041173+tyTheDebugger@users.noreply.github.com>
2020-03-04 21:18:23 -0600

    Update start.sh

--- web/start.sh
+++ web/start.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-xset -dpms
-xset s off
-xset s noblank
-
 unclutter &
 python3 -m http.server 8080 --bind 127.0.0.1 --cgi &
-chromium-browser localhost:8080 --window-size=320,480 --start-fullscreen --kiosk --incognito --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic
+sudo su - pi bash -c "chromium-browser localhost:8080 --window-size=320,480 --start-fullscreen --kiosk --incognito --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic"
