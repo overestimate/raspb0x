9d93378c78f6657f2e2c5a7d2ce88fd35b7bce96
Tyler Lawrence <58041173+tyTheDebugger@users.noreply.github.com>
2020-03-05 17:08:02 -0600

    edit the entire thing

--- web/cgi-bin/jailbreak.py
+++ web/cgi-bin/jailbreak.py
@@ -6,22 +6,11 @@ import cgi,cgitb,subprocess,os
 os.chdir("/home/pi/raspb0x")
 cgitb.enable() #for debugging
 print("Jailbreaking...")
-cdDone = subprocess.run(["cd", "/home/pi/raspb0x"], stdout=subprocess.DEVNULL)
-if (cdDone != 0):
-	print("Something failed, most likely broken install...")
-	print("<script>window.location='/index.html';")
 form = cgi.FieldStorage()
 if form.getvalue("verbose"):
-	doneJB = subprocess.run(["sudo", "./checkra1n","-V"], stdout=subprocess.PIPE)
+	subprocess.run(["sudo", "./checkra1n","-V","--wui","127.0.0.1","8081"], stdout=subprocess.PIPE)
 else:
-	doneJB = subprocess.run(["sudo", "./checkra1n"], stdout=subprocess.PIPE)
-for c in iter(lambda: process.stdout.read(15), b''):  # replace '' with b'' for Python 3
-        print(c)
-for x in range(15):
-	print("\033[F")
-if (doneJB != 0):
-	print("JB failed! Error code: %s" % str(doneJB))
-	print("<script>window.location='/index.html';</script>")
-else:
-	print("JB Success!")
-	print("<script>window.location='/index.html';</script>")
+	subprocess.run(["sudo", "./checkra1n","--wui","127.0.0.1","8081"], stdout=subprocess.PIPE)
+print("<script>window.location='http://localhost:8081';</script>")
+
+subprocess.run(["killall","chromium-browser"])
