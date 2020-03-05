8e0799d3bcdd4bfd1978ae17b7466a119a31e043
Tyler Lawrence <58041173+tyTheDebugger@users.noreply.github.com>
2020-03-05 17:12:29 -0600

    Update jailbreak.py

--- web/cgi-bin/jailbreak.py
+++ web/cgi-bin/jailbreak.py
@@ -7,10 +7,11 @@ os.chdir("/home/pi/raspb0x")
 cgitb.enable() #for debugging
 print("Jailbreaking...")
 form = cgi.FieldStorage()
+print("<script>window.location='http://localhost:8081';</script>")
 if form.getvalue("verbose"):
 	subprocess.run(["sudo", "./checkra1n","-V","--wui","127.0.0.1","8081"], stdout=subprocess.PIPE)
 else:
 	subprocess.run(["sudo", "./checkra1n","--wui","127.0.0.1","8081"], stdout=subprocess.PIPE)
-print("<script>window.location='http://localhost:8081';</script>")
+
 
 subprocess.run(["killall","chromium-browser"])
